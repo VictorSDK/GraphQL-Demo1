9095647420e7135671f6b562d098a2e9db18c45f
Victor SDK <victordevkit@gmail.com>
2023-06-15 12:35:08 -0600

    Implement authors and books

--- Query.cs
+++ Query.cs
@@ -1,4 +1,14 @@
 public class Query
 {
     public string Hello(string name = "World") => $"Hello {name}";
+
+    public IEnumerable<Book> GetBooks()
+    {
+        var author = new Author("Jon Skeet");
+        yield return new Book("C# in Depth", author);
+        yield return new Book("C# in Depth 2dn Edition", author);
+    }
 }
+
+public record Author(string name);
+public record Book(string title, Author author);
